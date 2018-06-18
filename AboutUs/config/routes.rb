d5254864c4438381ab5bdf6cd2fac649e6df2e34
Zac Henney <zachenney@sfs-wifi-dhcp-10-143-218-245.wifi.sfsu.edu>
2018-06-18 13:45:42 -0700

    adding routes and controller for dynamic paths

--- AboutUs/config/routes.rb
+++ AboutUs/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get 'startup/index'
+  get 'pages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#index'
+
+  get "/pages/:page" => "zac#show"
 end
