f406adc0df98a83b6693b2a4439dd440ef6cc344
iampaulanca <pancajima@mail.sfsu.edu>
2018-06-18 15:20:14 -0700

    updated controller files

--- AboutUs/app/controllers/pages_controller.rb
+++ AboutUs/app/controllers/pages_controller.rb
@@ -1,11 +1,4 @@
 class PagesController < ApplicationController
   def index
   end
-<<<<<<< HEAD
-=======
-
-  def show
-    render template: "pages/#{params[:page]}"
-  end
->>>>>>> b439d74558a4e86b847d5f52112aad3328342bf2
 end
--- AboutUs/config/routes.rb
+++ AboutUs/config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-	get 'pages/index'
-=======
+
   get 'pages/index'
->>>>>>> b439d74558a4e86b847d5f52112aad3328342bf2
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
-
-  get "/pages/:page" => "pages#show"
 end
