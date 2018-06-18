67b21d7c686ae99f3da3054d3d62d8ce67bf5271
iampaulanca <pancajima@mail.sfsu.edu>
2018-06-18 15:22:54 -0700

    updated routes.rb and pages_controller.rb

--- AboutUs/config/routes.rb
+++ AboutUs/config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
 
   get 'pages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
 end
-=======
-  get 'pages/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#index'
-end
->>>>>>> be348b67b0d9ac96a5f6db9d54b05cc22bc9c96f
