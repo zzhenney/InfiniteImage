b5ad16f3e3fb106fb3bb5e170ce7c29290342578
root <jphabmixay@gmail.com>
2018-07-07 13:03:35 -0700

    Added foreign key functionaility for tags. Changed tag_id column in database to belongs_to relation for tags as foreign keys. May have to run db:reset to purge data from local databases. Full details in Readme.md

--- AboutUs/app/models/image.rb
+++ AboutUs/app/models/image.rb
@@ -2,4 +2,7 @@ class Image < ApplicationRecord
   belongs_to :user, optional: true
   #sets up images for active storage
   has_many_attached :uploads
+
+  #foreign key set up
+  belongs_to :tag
 end
--- AboutUs/db/schema.rb
+++ AboutUs/db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_07_164448) do
+ActiveRecord::Schema.define(version: 2018_07_07_191822) do
 
-  create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
     t.bigint "record_id", null: false
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2018_07_07_164448) do
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
     t.string "content_type"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 2018_07_07_164448) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.integer "image_id"
     t.boolean "is_approved"
     t.string "image_title"
@@ -48,7 +48,13 @@ ActiveRecord::Schema.define(version: 2018_07_07_164448) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "user_id"
     t.boolean "is_admin"
     t.integer "cart_id"
