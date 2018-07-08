15cb73d0d3be937b601f67321f1070583565805a
root <jphabmixay@gmail.com>
2018-07-08 14:21:57 -0700

    Merged files with Paul's most recent serarch addition

--- AboutUs/db/schema.rb
+++ AboutUs/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_07_184301) do
+ActiveRecord::Schema.define(version: 2018_07_07_191822) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "name", null: false
