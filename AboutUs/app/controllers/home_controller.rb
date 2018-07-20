b1d55fc34f75c6d693768866f4be18b9efeefe1d
Zac Henney <zachenney@Zacs-MacBook-Pro-2.local>
2018-07-19 19:11:23 -0700

    hooking up search to database

--- AboutUs/app/controllers/home_controller.rb
+++ AboutUs/app/controllers/home_controller.rb
@@ -1,5 +1,15 @@
 class HomeController < ApplicationController
   def index
+    @home = Image.all
+
+    @q = Image.ransack(params[:q])          #Ransack gem's  Paul Ancajima
+    @home = @q.result(distinct: true)     #Simple search
+  end
+
+  def result
+    @home = Image.all
+    @q = Image.ransack(params[:q])          #Ransack gem's  Paul Ancajima
+    @home = @q.result(distinct: true)    #Simple search
   end
 
 end
