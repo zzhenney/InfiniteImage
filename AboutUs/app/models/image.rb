2ecbe2db279bf7ad7e09dc6fa0ecdf6a2ea3f1f9
dreles <andreleslie77@yahoo.com>
2018-07-24 18:19:35 -0700

    changes to image upload routing

--- AboutUs/app/models/image.rb
+++ AboutUs/app/models/image.rb
@@ -5,5 +5,14 @@ class Image < ApplicationRecord
   has_many_attached :uploads       #sets up images for active storage.     Paul ANcajima
   #var ":uploads" can be called anything must be accepted in params of controller IE. images_controller "
   #foreign key set up
-  belongs_to :category                   #inherits category @image.category.<etc>
+  belongs_to :category #inherits category @image.category.<etc>
+
+
+  def thumbnail select
+    return self.uploads[select].variant(resize: "300x300!").processed
+  end
+
+  def full_size select
+    return self.uploads[select].variant(resize: "700x700").processed
+  end
 end
