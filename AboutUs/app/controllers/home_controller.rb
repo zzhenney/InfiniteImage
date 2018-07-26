884838f3f7cb06370a2a9663ca28e8effff60b28
Zac Henney <zachenney@Zacs-MacBook-Pro-2.local>
2018-07-26 16:59:54 -0700

    Search: Category selection is now included in search

--- AboutUs/app/controllers/home_controller.rb
+++ AboutUs/app/controllers/home_controller.rb
@@ -1,31 +1,42 @@
 # Zac Henney, Paul Ancajima
 # Contoller for Home page views. Search is currently controlled here
 # Methods
-# index contains search function
-# parse_cat_id() - parses the user's search category id for persistent category selection
+# index contains search function and persistent category selection logic
+#
 
 
 class HomeController < ApplicationController
   def index
     @home = Image.all
 
-    @q = Image.ransack(params[:q])          #Ransack gem's
 
-    #get category selected by user for persistent selection
-    #at home page no search performed yet
+
+
+
+    # get category selected by user for persistent selection
+    # strip category and search params into variables @user_cat_id and @user_search
+    # this allows for the search function to search first through categories then for the search(image title)
+    # at home page no search performed yet
     if params[:q].nil? || params[:q][:category_id].nil?
       @user_cat_name = "All"
 
-    #if all selected check only that query (q) was made
+    # if 'All' selected check only that query (q) was made
     elsif params[:q].present?
       @user_cat_id = params[:q][:category_id] unless params[:q].nil?
-
-    #if specific category selected check that query (q) and category id exists
+      @user_search = params[:q][:image_title]
+    # if specific category selected check that query (q) and category id exists
     elsif params[:q][:category_id].present?
       @user_cat_name = Category.find(@user_cat_id).name unless params[:q].nil?
+      @user_search = params[:q][:image_title]
     end
 
-    @home = @q.result(distinct: true).includes(:category)     #Simple search
+    #Search function (ransack gem) - first searches categories then image title
+    @q = Image.ransack(category_id_eq: @user_cat_id, image_title_cont: @user_search)
+
+    #Set search result to @home instance variable for display
+    @home = @q.result
+
+
 
 
   end
