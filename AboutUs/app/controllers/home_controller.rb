c279ef5e56737764c0aeb6463b99cb2686b279ed
dreles <andreleslie77@yahoo.com>
2018-07-27 18:10:39 -0700

    photos will not be shown unless approved by admin

--- AboutUs/app/controllers/home_controller.rb
+++ AboutUs/app/controllers/home_controller.rb
@@ -7,40 +7,38 @@
 
 class HomeController < ApplicationController
   def index
+
     @home = Image.all
-
-
-
-
-
-    # get category selected by user for persistent selection
-    # strip category and search params into variables @user_cat_id and @user_search
-    # this allows for the search function to search first through categories then for the search(image title)
-    # at home page no search performed yet
-    if params[:q].nil? || params[:q][:category_id].nil?
-      @user_cat_name = "All"
-
-    # if 'All' selected check only that query (q) was made
-    elsif params[:q].present?
-      @user_cat_id = params[:q][:category_id] unless params[:q].nil?
-      @user_search = params[:q][:image_title]
-
-    # if specific category selected check that query (q) and category id exists
-    elsif params[:q][:category_id].present?
-      @user_cat_name = Category.find(@user_cat_id).name unless params[:q].nil?
-      @user_search = params[:q][:image_title]
-    end
-
-    #Search function (ransack gem) - first searches categories then image title
-    @q = Image.ransack(category_id_eq: @user_cat_id, image_title_cont: @user_search)
-
-    #Set search result to @home instance variable for display
-    @home = @q.result
-
-
+    if current_user != nil
     if current_user.is_admin
       redirect_to admin_path
     end
+    end
+      # get category selected by user for persistent selection
+      # strip category and search params into variables @user_cat_id and @user_search
+      # this allows for the search function to search first through categories then for the search(image title)
+      # at home page no search performed yet
+      if params[:q].nil? || params[:q][:category_id].nil?
+        @user_cat_name = "All"
+
+        # if 'All' selected check only that query (q) was made
+      elsif params[:q].present?
+        @user_cat_id = params[:q][:category_id] unless params[:q].nil?
+        @user_search = params[:q][:image_title]
+
+        # if specific category selected check that query (q) and category id exists
+      elsif params[:q][:category_id].present?
+        @user_cat_name = Category.find(@user_cat_id).name unless params[:q].nil?
+        @user_search = params[:q][:image_title]
+      end
+
+      #Search function (ransack gem) - first searches categories then image title
+      @q = Image.ransack(category_id_eq: @user_cat_id, image_title_cont: @user_search)
+
+      #Set search result to @home instance variable for display
+      @home = @q.result
+
+
 
 
 
